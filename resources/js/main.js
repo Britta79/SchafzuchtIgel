2c4798d51131f3faed7dbdc93b80c476fff055d7
Britta Owen <bowen@Scotts-iMac.local>
2018-12-20 18:47:42 +1000

    Version 1

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
